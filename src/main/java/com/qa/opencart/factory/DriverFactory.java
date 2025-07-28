18fc2ba38539754cd70b6fe71cff21e9d92ec2f8
pranathi <pranathimythrei@gmail.com>
2025-07-28 11:05:03 +0530

    latest code

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -92,20 +92,17 @@ public class DriverFactory {
 					break;
 				}
 			}
-			if (ip != null) {
-	            prop.load(ip);
-	        } else {
-	            throw new IllegalStateException("Properties file input stream is null.");
-	        }
-
-	    } catch (FileNotFoundException e) {
-	        log.error("Properties file not found for environment: " + envName, e);
-	    } catch (IOException e) {
-	        log.error("IO Exception while loading properties file", e);
-	    }
-
-	    return prop;
+			prop.load(ip);
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
 	}
+
 
 	public static File getScreenshotFile() {
 		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);// temp dir
